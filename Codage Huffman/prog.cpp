137519a7a418a7009b50b6ff8cd6b9f484d82bd1
loroberjot <loick.roberjot@etu.uca.fr>
2020-05-09 18:10:49 +0200

    update

--- Codage Huffman/prog.cpp
+++ Codage Huffman/prog.cpp
@@ -2,24 +2,39 @@
 #include "Node.h"
 using namespace std;
 
-void main()
+#include <iostream>
+#include <list>
+#include "Node.h"
+using namespace std;
+
+int main()
 {
 	string Text;
-	cout << "Entrez votre chaine de caractères à coder";
+	//string Reponse;
+	cout << "Entrez votre chaine de caractères à coder : " << endl;
 	cin >> Text;
 
 	string alphabet;
-	cout << "Entre ton alphabet #ergonomie pd, apprends l'ergonomie pd avec le professeur pédoncule";
+	cout << "Entre ton alphabet : " << endl;
 	cin >> alphabet;
-
-	cout << "La phrase codee est :";
-
+	//Reponse = HuffMan(Text, alphabet);
+	cout << "La phrase codee est : " << endl;
+	//cout<<Reponse;
 }
 
+string HuffMan(string Texte, string Alphabet) {
+	string codeHuffMan;
+	list<string> Text_tab;
+	list<CNode> m_lstNode;
+
+	for (size_t i = 0; i <= Texte.size(); i++) {
+		Text_tab.push_back(Texte.substr(i));
+	}
+	for (size_t j = 0; j <= Text_tab.size(); j++) { //affichage de la liste 
+		cout << "char n°" << j << " : " << Text_tab[j] << endl;
+	}
 
 
 
-
-
-
-
+	//m_lstNode.pushback(NewNode);
+	return codeHuffMan;
