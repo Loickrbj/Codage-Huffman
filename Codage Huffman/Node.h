f9e8bd810677fcf6985658d8ee5dc9d7433074ee
loroberjot <loick.roberjot@etu.uca.fr>
2020-05-10 21:50:42 +0200

    creation de l'arbre binaire (pas fini)

--- Codage Huffman/Node.h
+++ Codage Huffman/Node.h
@@ -4,30 +4,45 @@
 
 
 class CNode {
+
 private:
-	int proba;
-	std::string CodeHuffMan;
-	char lettre;
-	CNode* filsdroit;
-	CNode* filsgauche;
-
-
-	
+	//Creation des variables:
+	int proba; //nombre apparition lettre
+	std::string CodeHuffMan; // code HuffMan de la lettre
+	char lettre; // c'est la lettre
+	//Les enfants, ces connards:
+	CNode* filsdroit; // pointeur vers le fils droit
+	CNode* filsgauche; //Pointeur vers le fils gauche
 
 public:
-	CNode(char lalettre, int lafrequence);
+
+	
+	//Declaration du constructeur:
+	CNode( char lalettre, int lafrequence);
+	CNode() {};
+
+	//Getteurs:
 	char Getlettre() { return lettre; }
 	std::string GetCodeHuffMan() { return CodeHuffMan; }
+	int GetProba() { return proba; }
+	CNode* GetFilsDroit() { return filsdroit; }
+	CNode* GetFilsGauche() { return filsgauche; }
+
+	//Setteurs
 	void Setlettre(char new_lettre) { lettre = new_lettre; }
-	void SetCodeHuffMan(std::string code){CodeHuffMan=code;}
+	void SetCodeHuffMan(std::string code) { CodeHuffMan = code; }
+	void SetProba(int new_proba) { proba = new_proba; }
+	void SetFilsDroit(CNode* FilsD) { filsdroit = FilsD; }
+	void SetFilsGauche(CNode* FilsG) { filsgauche = FilsG; }
+
+	//void operator+(int prob) {}
 };
 
-
+//Definiton du constructeur:
 CNode::CNode(char lalettre, int lafrequence)
 {
 	lettre = lalettre;
 	proba = lafrequence;
-
 }
 
 
