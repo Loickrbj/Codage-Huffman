04ec31c3c0698658e3f12c191ad79c7b96db00d1
loroberjot <loick.roberjot@etu.uca.fr>
2020-05-11 22:28:31 +0200

    code qui ne fonctionne pas  (problème sur fils droit et gauche)

---
+++ Codage Huffman/Node.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class CNode {
+
+private:
+	//Creation des variables:
+	int proba; //nombre apparition lettre
+	std::string CodeHuffMan; // code HuffMan de la lettre
+	std::string lettre; // c'est la lettre
+	CNode* filsdroit =nullptr; // pointeur vers le fils droit
+	CNode* filsgauche=nullptr; //Pointeur vers le fils gauche
+	CNode* pere= nullptr; //pointeur v	using pNode
+	//std::shared_ptr<CNode>;
+
+public:
+
+	
+	//Declaration du constructeur:
+	CNode(std::string lalettre, std::string lettrefilsgauche, std::string lettrefilsdroit, int lafrequence);
+	CNode() {};
+
+	//Getteurs:
+	std::string Getlettre() { return lettre; }
+	std::string GetCodeHuffMan() { return CodeHuffMan; }
+	int GetProba() { return proba; }
+	CNode* GetFilsDroit() { return filsdroit; }
+	CNode* GetFilsGauche() { return filsgauche; }
+	CNode* GetPere() { return pere; }
+
+	//Setteurs
+	void Setlettre(char new_lettre) { lettre = new_lettre; }
+	void SetCodeHuffMan(std::string code) { CodeHuffMan = code; }
+	void SetProba(int new_proba) { proba = new_proba; }
+	void SetFilsDroit(CNode* FilsD) { filsdroit = FilsD; }
+	void SetFilsGauche(CNode* FilsG) { filsgauche = FilsG; }
+	void Setpere(CNode* papa){pere = papa;}
+	bool Has_Fils();
+	void PushbackHuffman(char code) { CodeHuffMan.push_back(code); };
+};
+
+//Definiton du constructeur:
+CNode::CNode(std::string lalettre,std::string lettrefilsgauche, std::string lettrefilsdroit, int lafrequence)
+{
+	lettre = lalettre;
+	lettre += lettrefilsgauche;
+	lettre += lettrefilsdroit;
+	proba = lafrequence;
+
+}
+
+bool CNode::Has_Fils()
+{
+	if (GetFilsDroit() == nullptr)
+	{
+		std::cout << "NO HAS FILS\n";
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+
+};
--- Codage Huffman/prog.cpp
+++ Codage Huffman/prog.cpp
@@ -2,8 +2,77 @@
 #include <vector>
 #include<list>
 #include <algorithm>
-#include "Node.h"
+#include "Node.hpp"
 using namespace std;
+
+
+int byWeight(CNode a,CNode b)
+{
+	if(a.GetProba()>b.GetProba())
+	{
+		return 1;
+	}
+	else{
+		if (a.GetProba() > b.GetProba())
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
+/*vector<CNode> tri(vector<CNode> m_lstNode)
+{
+	unsigned int i,j;
+	CNode c;
+	for(i=0;i<m_lstNode.size()-1;i++)
+    for( j=i+1;j<m_lstNode.size();j++)
+        if ( m_lstNode[i].GetProba() > m_lstNode[j].GetProba() ) {
+            c = m_lstNode[i];
+            m_lstNode[i] = m_lstNode[j];
+            m_lstNode[j] = c;
+        }
+	reverse(m_lstNode.begin(), m_lstNode.end());
+	return m_lstNode;
+}*/
+
+string Find_Letter(CNode* Noeud, char lettre, string alphabet)
+{
+	cout<<"Test: "<<Noeud->GetFilsGauche()->GetFilsDroit()->Getlettre()<<endl;
+	bool CheckGauche=false;
+	string Huff;
+	int compteurv2 = 0;
+	while (Noeud->GetFilsGauche()!=nullptr)
+	{
+
+		for (auto car : Noeud->GetFilsGauche()->Getlettre()) {
+
+			if (car == lettre) {
+				CheckGauche = true;
+			}
+		}
+
+		if (CheckGauche) {
+			//insérer commande Huffman '1'
+			Noeud = Noeud->GetFilsGauche();
+			Huff += '0';
+
+		}
+		else {
+			//insérer commande Huffman '0'
+			Noeud = Noeud->GetFilsDroit();
+			Huff += '1';
+		}
+		CheckGauche = false;
+		Huff += ' ';
+		compteurv2++;
+		return Huff; // return ici car sinon boucle infini et on peut rien voir 
+	}
+
+}
 
 string HuffMan(string Texte, string Alphabet)
 {
@@ -16,7 +85,7 @@ string HuffMan(string Texte, string Alphabet)
 	string alphab;
 	CNode filsdroit;
 	CNode filsgauche;
-	//list<CNode> Arbre;
+
 
 	for (unsigned int j = 0; j < alphabet.size(); ++j)
 	{ //affichage de la liste de l'alphabet'
@@ -24,9 +93,7 @@ string HuffMan(string Texte, string Alphabet)
 		Text_Proba.push_back(count(Text_tab.begin(), Text_tab.end(), alphabet[j]));
 		alphab.push_back(alphabet[j]);
 	}
-
-	//sort(Text_Compt.begin(), Text_Compt.end());
-
+	
 	for (auto a : Text_Proba)
 	{
 		cout << a;
@@ -35,76 +102,65 @@ string HuffMan(string Texte, string Alphabet)
 	{
 		string alpha;
 		alpha.push_back(alphab[i]);
-		CNode Node(alpha, Text_Proba[i]);
+		CNode Node(alpha,"","", Text_Proba[i]);
 		m_lstNode.push_back(Node);
 	}
 	//trie par ordre decroissant
-	unsigned int i,j;
-	CNode c;
-	for(i=0;i<m_lstNode.size()-1;i++)
-    for( j=i+1;j<m_lstNode.size();j++)
-        if ( m_lstNode[i].GetProba() > m_lstNode[j].GetProba() ) {
-            c = m_lstNode[i];
-            m_lstNode[i] = m_lstNode[j];
-            m_lstNode[j] = c;
-        }
-	reverse(m_lstNode.begin(), m_lstNode.end());
-	//sort(m_lstNode.begin(), m_lstNode.end());
-	cout << endl << "apres tri: " << endl;
+	//tri(m_lstNode);
+	
+
+	//------------------------------------------------------
+	while (m_lstNode.size() >= 2)
+	{
+		sort(m_lstNode.begin(), m_lstNode.end(), byWeight);
+		//m_lstNode=tri(m_lstNode);
+		/*cout << endl << "apres tri: " << endl;
 	for (auto a : m_lstNode)
 	{
 		cout << a.GetProba();
-	}
-	while (m_lstNode.size() >= 2)
-	{
+	}*/
+		//cout << "AVANT\n";
 		//prendre les deux dernier noeuds et les supprimer de la liste
+		/*for(auto i: m_lstNode)
+		{
+			cout << endl << i.Getlettre();
+		}*/
 		filsdroit = m_lstNode.back();
 		m_lstNode.pop_back();
 		filsgauche = m_lstNode.back();
 		m_lstNode.pop_back();
-
+		//cout << "APRES\n";
+		for(auto i: m_lstNode)
+		{
+			cout << endl << i.Getlettre() << endl;
+		}
 		//créer un nouveau noeud père qui aura pour fils les deux noeuds précédemment supprimés
-		CNode Node(filsdroit.Getlettre() + filsgauche.Getlettre(), filsdroit.GetProba() + filsgauche.GetProba());
+		CNode Node("",filsdroit.Getlettre() , filsgauche.Getlettre(), filsdroit.GetProba() + filsgauche.GetProba());
 		cout << endl << Node.Getlettre()<<endl;
+		//cout << endl << Node.GetProba()<<endl;
 		cout << "-----------------------\n";
 		Node.SetFilsDroit(&filsdroit);
 		Node.SetFilsGauche(&filsgauche);
-		//mettre un code à ces deux fils (0 pour le fils droit à plus faible probabilité et 1 pour l'autre)
-		filsdroit.SetCodeHuffMan("0");
-		filsgauche.SetCodeHuffMan("1");
-
+		cout << "Fils droit"<<endl;
+		cout<<Node.GetFilsDroit()->Getlettre()<<endl;
+		cout << "Fils gauche"<<endl;
+		cout << Node.GetFilsGauche()->Getlettre()<<endl;
+		
 		//ajouter le noeud père à la liste
 		m_lstNode.push_back(Node);
-		//Arbre.push_back(Node);
 		
 	}
+	
+
 	//chercher chaque lettre dans l'arbre binaire
-	
-	CNode Teste = m_lstNode.back();
-	CNode FDroit();
-	CNode FGauche();
-	while(Teste.Has_Fils())
-	{
-		Teste.GetFilsDroit();
-		Teste.GetFilsGauche();
-	}
-	
-
-	//for (auto i : Arbre)
-	//{
-
-		//tant que le noeud de la lettre qu'on cherche a pas été trouvé :
-		/*while(Texte[int j = 0] == m_lstNode[int i= 0].Getlettre() && m_lstNode[i].size()== 1)
-		{
-		for()
-
-		j++;
-		}
-		*/
-	//}
-	//return phrase_codee;
-	return codeHuffMan;
+	CNode* Noeud= &m_lstNode.back();
+	string code;
+	code = Find_Letter(Noeud, 'p', alphabet);
+	return code;
 }
+
+
+
 
 int main()
 {
